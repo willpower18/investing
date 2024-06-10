2c52ed755390e509536a315c592806d4b4231aa5
willpower18 <willjcpower@gmail.com>
2024-06-10 09:49:45 -0300

    desenvolvimento parcial da interface de configuracao de carteira

---
+++ Investing.WebApp/Models/WalletPercentage.cs
@@ -0,0 +1,10 @@
+﻿namespace Investing.WebApp.Models
+{
+    public class WalletPercentage
+    {
+        public int AssetClassId { get; set; }
+        public int Line { get; set; }
+        public string Name { get; set; }
+        public double Percentage { get; set; }
+    }
+}
